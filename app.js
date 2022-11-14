27b2e7ade1fdae85a76f896c08ecf32eca0bfe27
VCarbonell <v.carbonell+gh@hotmail.fr>
2022-11-14 10:41:03 +0100

    express03

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -16,9 +17,11 @@ const userHandlers = require('./userHandlers');
 
 app.get('/api/movies', movieHandlers.getMovies);
 app.get('/api/movies/:id', movieHandlers.getMovieById);
-
 app.get('/api/users', userHandlers.getUsers);
 app.get('/api/users/:id', userHandlers.getUsersById);
+
+app.post('/api/movies', movieHandlers.postMovies);
+app.post('/api/users', userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -51,13 +51,33 @@ const getMovieById = (req, res) => {
         res.json(result);
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).send('Error retrieving data from database');
+    });
+};
+
+const postMovies = (req, res) => {
+  const {
+    title, director, year, color, duration,
+  } = req.body;
+
+  database
+    .query(
+      'INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+      [title, director, year, color, duration],
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error saving the movie');
     });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovies,
 };
